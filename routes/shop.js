8d1bd3cd6b3428d335830d883415faa2b65c5bd9
araignee19 <julie.araignee@gmail.com>
2022-06-01 15:51:01 +0200

    added avi buying & equipping

--- routes/shop.js
+++ routes/shop.js
@@ -8,4 +8,20 @@ router.get('/', authCheck.checkAuthenticated, async (req, res) => {
     res.render('shop/shop', { user: req.user, prices: prices})
 })
 
+router.post('/:id', authCheck.checkAuthenticated, async (req, res) => {
+    try {
+        const user = await User.findById(req.user)
+        user.coins -= prices[req.params.id]
+        user.unlockedSkins.push(req.params.id)
+        
+        const savedUser = await user.save()
+        res.render('shop/shop', {
+            user: savedUser,
+            prices: prices
+        })
+    } catch {
+        res.redirect('shop/shop')
+    }
+})
+
 module.exports = router
