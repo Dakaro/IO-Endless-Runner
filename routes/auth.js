ff7d434e6ede45f9df271df81867f54112d820ce
Oolcik <80123631+Oolcik@users.noreply.github.com>
2022-06-08 17:03:00 +0200

    password encryption

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
-<<<<<<< HEAD
 const authCheck = require('../authCheck')
 const bcrypt = require('bcrypt')
-=======
-const utils = require('../utils')
->>>>>>> fa60aa09bbe4a953bf11b2df38d01c3a595e5858
 
 router.get('/', utils.checkNotAuthenticated, (req, res) => {
     res.render('auth/login')
@@ -18,16 +14,7 @@ router.get('/register', utils.checkNotAuthenticated, (req, res) => {
     })
 })
 
-<<<<<<< HEAD
 router.post('/register', authCheck.checkNotAuthenticated, async (req, res) => {   
-=======
-router.post('/register', utils.checkNotAuthenticated, async (req, res) => {
-    const user = new User({
-        username: req.body.username,
-        password: req.body.password
-    })
-    
->>>>>>> fa60aa09bbe4a953bf11b2df38d01c3a595e5858
     try {
         const hashpassword = await bcrypt.hash(req.body.password, 10)
         const user = new User({
@@ -38,15 +25,11 @@ router.post('/register', utils.checkNotAuthenticated, async (req, res) => {
         console.log(newUser)
         res.redirect('/')
     } catch {
-<<<<<<< HEAD
         res.render('auth', {
             user: {
                 username: req.body.username,
                 password: hashpassword  //req.body.password
             },
-=======
-        res.render('auth/register', {
->>>>>>> fa60aa09bbe4a953bf11b2df38d01c3a595e5858
             errorMessage: 'Error registering new user'
         })
     }
