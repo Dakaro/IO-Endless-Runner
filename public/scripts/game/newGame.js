d7067508c0b326bc87c5a75bd0f98447296fb986
Dakaro <30911232+Dakaro@users.noreply.github.com>
2022-06-12 02:39:27 +0200

    final with factory

--- public/scripts/game/newGame.js
+++ public/scripts/game/newGame.js
@@ -26,7 +26,7 @@ var scoreEl = document.getElementById("score");
 var hearthsEl = document.querySelectorAll(".hearth");
 // LITERALS
 var MIN_COIN_DELAY = 2000;
-var MAX_COIN_DELAY = 7000;
+var MAX_COIN_DELAY = 5000;
 var MIN_ENEMY_DELAY = 1000;
 var MAX_ENEMY_DELAY = 4000;
 var LIVES_NUMBER = 3;
@@ -38,7 +38,6 @@ var GAME_ON = 1;
 // KEYS
 var SPACE = 32;
 var UP = 38;
-var coins = 0;
 var setRandomInterval = function (intervalFunction, minDelay, maxDelay) {
     var timeout;
     var runInterval = function () {
@@ -54,54 +53,42 @@ var setRandomInterval = function (intervalFunction, minDelay, maxDelay) {
         clear: function () { clearTimeout(timeout); },
     };
 };
-var Entity = /** @class */ (function () {
-    function Entity() {
+//enemy concrete factory
+var EnemyFactory = /** @class */ (function () {
+    function EnemyFactory(MIN_DELAY, MAX_DELAY) {
+        this.MIN_DELAY = MIN_DELAY;
+        this.MAX_DELAY = MAX_DELAY;
+    }
+    EnemyFactory.prototype.CreateEntity = function () {
+        return new Enemy();
+    };
+    return EnemyFactory;
+}());
+//coin concrete facotry
+var CoinFactory = /** @class */ (function () {
+    function CoinFactory(MIN_DELAY, MAX_DELAY) {
+        this.MIN_DELAY = MIN_DELAY;
+        this.MAX_DELAY = MAX_DELAY;
+    }
+    CoinFactory.prototype.CreateEntity = function () {
+        return new Coin();
+    };
+    return CoinFactory;
+}());
+//abstract entity
+var AbstractEntity = /** @class */ (function () {
+    function AbstractEntity() {
         this.position = 750;
         this.ownSpeed = SPEED;
     }
-    Entity.prototype.setPosition = function () {
+    AbstractEntity.prototype.changePosition = function () {
         this.position += this.ownSpeed;
         this.entityDiv.style.left = this.position + 'px';
     };
-    Entity.prototype.remove = function () {
+    AbstractEntity.prototype.remove = function () {
         this.entityDiv.remove();
     };
-    return Entity;
-}());
-var Coin = /** @class */ (function (_super) {
-    __extends(Coin, _super);
-    function Coin() {
-        var _this = _super.call(this) || this;
-        _this.entityDiv = document.createElement("div");
-        _this.entityDiv.classList.add("coin");
-        containerEl.appendChild(_this.entityDiv);
-        return _this;
-    }
-    Coin.prototype.checkCollision = function (playerPosition) {
-        return this.position < 70 && this.position > 20 && playerPosition >= 60;
-    };
-    Coin.prototype.grabCoin = function () {
-        var _this = this;
-        if (!this.entityDiv.classList.contains("grabItem")) {
-            coinIconEl.classList.add("coinJump");
-            coins++;
-            scoreAmountEl.innerHTML = coins;
-        }
-        this.entityDiv.classList.add("grabItem");
-        setTimeout(function () { _this.remove(); coinIconEl.classList.remove("coinJump"); }, 200);
-    };
-    return Coin;
-}(Entity));
-var CoinsGenerator = /** @class */ (function () {
-    function CoinsGenerator() {
-    }
-    CoinsGenerator.generateCoins = function (coinsArray) {
-        return setRandomInterval(function () {
-            var newCoin = new Coin();
-            coinsArray.push(newCoin);
-        }, MIN_COIN_DELAY, MAX_COIN_DELAY);
-    };
-    return CoinsGenerator;
+    return AbstractEntity;
 }());
 var Enemy = /** @class */ (function (_super) {
     __extends(Enemy, _super);
@@ -116,23 +103,43 @@ var Enemy = /** @class */ (function (_super) {
         return this.position < 70 && this.position > 20 && playerPosition <= 70;
     };
     return Enemy;
-}(Entity));
-var EnemyGenerator = /** @class */ (function () {
-    function EnemyGenerator() {
+}(AbstractEntity));
+var Coin = /** @class */ (function (_super) {
+    __extends(Coin, _super);
+    function Coin() {
+        var _this = _super.call(this) || this;
+        _this.entityDiv = document.createElement("div");
+        _this.entityDiv.classList.add("coin");
+        containerEl.appendChild(_this.entityDiv);
+        return _this;
     }
-    EnemyGenerator.generateEnemies = function (enemyArray) {
-        return setRandomInterval(function () {
-            var newEnemy = new Enemy();
-            enemyArray.push(newEnemy);
-        }, MIN_ENEMY_DELAY, MAX_ENEMY_DELAY);
+    Coin.prototype.checkCollision = function (playerPosition) {
+        return this.position < 70 && this.position > 20 && playerPosition >= 60;
     };
-    return EnemyGenerator;
+    return Coin;
+}(AbstractEntity));
+var GenerateEntity = /** @class */ (function () {
+    function GenerateEntity(factory) {
+        this.factory = factory;
+    }
+    GenerateEntity.prototype.generateEntity = function (entityArray) {
+        var _this = this;
+        return setRandomInterval(function () {
+            var containerList = containerEl.children;
+            if (!(containerList[containerList.length - 2].className == 'coin') && !(containerList[containerList.length - 2].className == 'enemy')) {
+                var newEntity = _this.factory.CreateEntity();
+                entityArray.push(newEntity);
+            }
+        }, this.factory.MIN_DELAY, this.factory.MAX_DELAY);
+    };
+    return GenerateEntity;
 }());
 var Player = /** @class */ (function () {
     function Player() {
         this.playerDiv = document.getElementById("hero");
         this.position = 0;
         this.distance = 0;
+        this.coins = 0;
         this.lives = LIVES_NUMBER;
         this.immune = false;
     }
@@ -140,6 +147,7 @@ var Player = /** @class */ (function () {
         var _this = this;
         if (!this.playerDiv.classList.contains("goJump")) {
             this.playerDiv.classList.add("goJump");
+            // dividing time to better collision wtih enemy managment
             setTimeout(function () { _this.position = 100; }, 100);
             setTimeout(function () { _this.position = 0; }, 500);
             setTimeout(function () { _this.playerDiv.classList.remove("goJump"); }, 600);
@@ -149,12 +157,21 @@ var Player = /** @class */ (function () {
         var _this = this;
         if (this.lives > 0 && this.position == 0 && !this.immune) {
             this.immune = true;
+            setTimeout(function () { return _this.immune = false; }, IMMUNITY_DURATION);
             --this.lives;
             hearthsEl[this.lives].classList.add("grabItem");
             containerEl.classList.add("hit");
             setTimeout(function () { hearthsEl[_this.lives].remove(); containerEl.classList.remove("hit"); }, 200);
-            setTimeout(function () { return _this.immune = false; }, IMMUNITY_DURATION);
         }
+    };
+    Player.prototype.grabCoin = function (coin) {
+        if (!coin.entityDiv.classList.contains("grabItem")) {
+            coinIconEl.classList.add("coinJump");
+            this.coins++;
+            scoreAmountEl.innerHTML = this.coins;
+        }
+        coin.entityDiv.classList.add("grabItem");
+        setTimeout(function () { coin.remove(); coinIconEl.classList.remove("coinJump"); }, 200);
     };
     return Player;
 }());
@@ -181,14 +198,14 @@ var GameEngine = /** @class */ (function () {
         var _this = this;
         this.coins.forEach(function (coin, index) {
             // Move each coin to the left and remove if outside the box
-            coin.setPosition();
+            coin.changePosition();
             if (coin.position <= -50) {
                 _this.coins.splice(index, 1);
                 coin.remove();
             }
             // Check player hitbox
             if (coin.checkCollision(playerPosition)) {
-                coin.grabCoin();
+                _this.player.grabCoin(coin);
             }
         });
     };
@@ -196,7 +213,7 @@ var GameEngine = /** @class */ (function () {
         var _this = this;
         this.enemies.forEach(function (enemy, index) {
             // Move each enemy to the left and remove if outside the box
-            enemy.setPosition();
+            enemy.changePosition();
             if (enemy.position <= -50) {
                 _this.enemies.splice(index, 1);
                 enemy.remove();
@@ -210,6 +227,7 @@ var GameEngine = /** @class */ (function () {
         // Jump on key press
         if (this.controller.keysInput.up)
             this.player.jump();
+        // check game over
         if (this.player.lives == 0) {
             this.endGame();
             this.player.lives = -1;
@@ -257,9 +275,10 @@ var GameEngine = /** @class */ (function () {
         setTimeout(function () { _this.countDistance(); }, 3000);
         setTimeout(function () {
             window.requestAnimationFrame(function () { return _this.gameLoop(Date.now()); });
-            _this.coinGeneratorInterval = CoinsGenerator.generateCoins(_this.coins);
-            _this.enemyGeneratorInterval = EnemyGenerator.generateEnemies(_this.enemies);
-        }, 3000);
+            var coinFactory = new CoinFactory(MIN_COIN_DELAY, MAX_COIN_DELAY);
+            _this.coinGeneratorInterval = new GenerateEntity(coinFactory).generateEntity(_this.coins);
+            _this.enemyGeneratorInterval = new GenerateEntity(new EnemyFactory(MIN_ENEMY_DELAY, MAX_ENEMY_DELAY)).generateEntity(_this.enemies);
+        }, 2000);
     };
     GameEngine.prototype.endGame = function () {
         GAME_ON = 0;
@@ -274,15 +293,14 @@ var GameEngine = /** @class */ (function () {
         scoreEl.style.display = "none";
         countdownEl.style.fontSize = "40px";
         var finalDistance = this.player.distance;
-        var finalScore = coins;
+        var finalScore = this.player.coins;
         countdownEl.innerHTML =
             "Final distance: ".concat(finalDistance, "m") +
                 "<br />" +
                 "Collected coins: ".concat(finalScore);
-            this.sendResultToDb(finalScore, finalDistance)
+          this.sendResultToDb(finalScore, finalDistance)
     };
-    // post request with coins and points update after game over
-    // updates sidenav coin value
+    // push result to DB
     GameEngine.prototype.sendResultToDb = function (coins, distance) {
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function () {
--- public/scripts/game/newGame.ts
+++ public/scripts/game/newGame.ts
@@ -12,7 +12,7 @@ let hearthsEl = document.querySelectorAll(".hearth");
 
 // LITERALS
 const MIN_COIN_DELAY = 2000
-const MAX_COIN_DELAY = 7000
+const MAX_COIN_DELAY = 5000
 
 const MIN_ENEMY_DELAY = 1000
 const MAX_ENEMY_DELAY = 4000
@@ -24,12 +24,11 @@ const MAX_SPEED = -19
 const ACCELERATION = -0.001
 let SPEED = -6
 let GAME_ON = 1
+
+
 // KEYS
 const SPACE = 32
 const UP = 38
-
-let coins = 0
-
 
 const setRandomInterval = (intervalFunction, minDelay, maxDelay) => {
     let timeout;
@@ -47,77 +46,70 @@ const setRandomInterval = (intervalFunction, minDelay, maxDelay) => {
     };
 };
 
+//entity abstract factory
+interface EntityFactory {
+    MIN_DELAY: number;
+    MAX_DELAY: number;
+    CreateEntity(): AbstractEntity;
+}
 
-abstract class Entity {
-    position: number
-    entityDiv: HTMLDivElement
-    entityArray: Array
+//enemy concrete factory
+class EnemyFactory implements EntityFactory {
+    MIN_DELAY: number;
+    MAX_DELAY: number;
+    constructor(MIN_DELAY: number, MAX_DELAY: number) {
+        this.MIN_DELAY = MIN_DELAY;
+        this.MAX_DELAY = MAX_DELAY;
+    }
+    public CreateEntity(): AbstractEntity {
+        return new Enemy();
+    }
+}
+
+//coin concrete facotry
+class CoinFactory implements EntityFactory {
+    MIN_DELAY: number;
+    MAX_DELAY: number;
+    constructor(MIN_DELAY: number, MAX_DELAY: number) {
+        this.MIN_DELAY = MIN_DELAY;
+        this.MAX_DELAY = MAX_DELAY;
+    }
+    CreateEntity(): AbstractEntity {
+        return new Coin();
+    }
+}
+
+//abstract entity
+abstract class AbstractEntity {
+    position: number;
+    entityDiv: HTMLDivElement;
     ownSpeed: number
 
-    constructor(){
-        this.position = 750
+    constructor() {
+        this.position = 750;
         this.ownSpeed = SPEED
     }
 
-    setPosition(){
-        this.position += this.ownSpeed
-        this.entityDiv.style.left = this.position + 'px'
+    public changePosition() {
+        this.position += this.ownSpeed;
+        this.entityDiv.style.left = this.position + 'px';
     }
 
     remove(){
-        this.entityDiv.remove()
+        this.entityDiv.remove();
     }
 
-    abstract checkCollision(playerPosition: number): boolean
+    abstract checkCollision(playerPosition: number): boolean;
+
 }
 
-
-class Coin extends Entity{
-    position: number
-    entityDiv: HTMLDivElement
+class Enemy extends AbstractEntity {
 
     constructor(){
         super()
-        this.entityDiv = document.createElement("div")
-        this.entityDiv.classList.add("coin")
-        containerEl.appendChild(this.entityDiv)
-    }
-
-    checkCollision(playerPosition: number): boolean{
-        return this.position < 70 && this.position > 20 && playerPosition >= 60
-    }
-
-    grabCoin(): any{
-
-          if( !this.entityDiv.classList.contains("grabItem") ){
-            coinIconEl.classList.add("coinJump");
-            coins++;
-            scoreAmountEl.innerHTML = coins;
-          }
-          this.entityDiv.classList.add("grabItem")
-          setTimeout(() => { this.remove(); coinIconEl.classList.remove("coinJump"); }, 200);
-    }
-
-}
-
-class CoinsGenerator{
-    static generateCoins(coinsArray): {clear: () => void}{
-        return setRandomInterval(() => {
-            let newCoin = new Coin()
-            coinsArray.push(newCoin)
-        }, MIN_COIN_DELAY, MAX_COIN_DELAY)
-    }
-}
-
-class Enemy extends Entity{
-    position: number
-    entityDiv: HTMLDivElement
-
-    constructor(){
-        super()
-        this.entityDiv = document.createElement("div")
-        this.entityDiv.classList.add("enemy")
-        containerEl.appendChild(this.entityDiv)
+        this.entityDiv = document.createElement("div");
+        this.entityDiv.classList.add("enemy");
+        containerEl.appendChild(this.entityDiv);
     }
 
     checkCollision(playerPosition: number): boolean{
@@ -125,55 +117,89 @@ class Enemy extends Entity{
     }
 }
 
-class EnemyGenerator {
-    static generateEnemies(enemyArray): {clear: () => void}{
-        return setRandomInterval(() => {
-            let newEnemy = new Enemy()
-            enemyArray.push(newEnemy)
-        }, MIN_ENEMY_DELAY, MAX_ENEMY_DELAY)
+class Coin extends AbstractEntity {
+    constructor(){
+        super()
+        this.entityDiv = document.createElement("div");
+        this.entityDiv.classList.add("coin");
+        containerEl.appendChild(this.entityDiv);
+    }
+
+    checkCollision(playerPosition: number): boolean{
+        return this.position < 70 && this.position > 20 && playerPosition >= 60
     }
 }
 
+class GenerateEntity {
+    factory: EntityFactory;
+
+    constructor(factory: EntityFactory) {
+        this.factory = factory;
+    }
+
+    public generateEntity(entityArray): { () => void} {
+        return setRandomInterval(() => {
+          let containerList = containerEl.children
+          if( !(containerList[ containerList.length - 2 ].className == 'coin') && !(containerList[ containerList.length - 2 ].className == 'enemy') ){
+            let newEntity = this.factory.CreateEntity();
+            entityArray.push(newEntity)
+          }
+        },
+        this.factory.MIN_DELAY, this.factory.MAX_DELAY)
+      }
+
+}
+
 class Player {
-    playerDiv: HTMLDivElement
+    playerDiv: HTMLElement
     position: number
     lives: number
     immune: boolean
     distance: number
+    coins: number
 
     constructor(){
         this.playerDiv = document.getElementById("hero");
         this.position = 0
         this.distance = 0
+        this.coins = 0
         this.lives = LIVES_NUMBER
         this.immune = false
     }
 
-
-
     jump(): any {
       if ( !this.playerDiv.classList.contains("goJump") ) {
           this.playerDiv.classList.add("goJump");
+
+          // dividing time to better collision wtih enemy managment
           setTimeout( () => {this.position = 100 }, 100);
           setTimeout(() => { this.position = 0 }, 500);
           setTimeout( () => {this.playerDiv.classList.remove("goJump")} , 600);
-        }
-    }
+        }    }
 
     getHit(): any {
+      if (this.lives > 0 && this.position == 0 && !this.immune ) {
+          this.immune = true
+          setTimeout(() => this.immune = false, IMMUNITY_DURATION)
 
-        if (this.lives > 0 && this.position == 0 && !this.immune ) {
-            this.immune = true
-            --this.lives
-
-            hearthsEl[this.lives].classList.add("grabItem");
-            containerEl.classList.add("hit");
-            setTimeout(() => { hearthsEl[this.lives].remove(); containerEl.classList.remove("hit") }, 200);
-
-
-            setTimeout(() => this.immune = false, IMMUNITY_DURATION)
-        }
+          --this.lives
+          hearthsEl[this.lives].classList.add("grabItem");
+          containerEl.classList.add("hit");
+          setTimeout(() => { hearthsEl[this.lives].remove(); containerEl.classList.remove("hit") }, 200);
+      }
     }
+
+    grabCoin(coin: Coin): any{
+
+          if( !coin.entityDiv.classList.contains("grabItem") ){
+            coinIconEl.classList.add("coinJump");
+            this.coins++;
+            scoreAmountEl.innerHTML = this.coins;
+          }
+          coin.entityDiv.classList.add("grabItem")
+          setTimeout(() => { coin.remove(); coinIconEl.classList.remove("coinJump"); }, 200);
+    }
+
 }
 
 
@@ -192,7 +218,6 @@ class Controller {
 
     keyDownHandler(e){
         if ( e.keyCode == UP || e.keyCode == SPACE ) this.keysInput.up = true
-    }
 }
 
 
@@ -207,7 +232,7 @@ class GameEngine {
     coinsUpdate(playerPosition: number): any {
         this.coins.forEach((coin, index) => {
             // Move each coin to the left and remove if outside the box
-            coin.setPosition()
+            coin.changePosition()
             if (coin.position <= -50) {
                 this.coins.splice(index, 1)
                 coin.remove()
@@ -215,7 +240,7 @@ class GameEngine {
 
             // Check player hitbox
             if( coin.checkCollision(playerPosition) ) {
-              coin.grabCoin()
+              this.player.grabCoin(coin)
             }
 
         })
@@ -224,7 +249,7 @@ class GameEngine {
     enemiesUpdate(playerPosition: number): any {
         this.enemies.forEach((enemy, index) => {
             // Move each enemy to the left and remove if outside the box
-            enemy.setPosition()
+            enemy.changePosition()
             if (enemy.position <= -50) {
                 this.enemies.splice(index, 1)
                 enemy.remove()
@@ -233,25 +258,22 @@ class GameEngine {
             // Check hitbox
             if(enemy.checkCollision(playerPosition)) this.player.getHit()
         })
-   }
-
+    }
 
     update(timeStamp: number): any{
+      // Jump on key press
+      if(this.controller.keysInput.up ) this.player.jump()
 
-        // Jump on key press
-        if(this.controller.keysInput.up ) this.player.jump()
+      // check game over
+      if( this.player.lives == 0 ){
+        this.endGame();
+        this.player.lives = -1
+      }
 
-        if( this.player.lives == 0 ){
-          this.endGame();
-          this.player.lives = -1
-        }
+      this.coinsUpdate(this.player.position)
+      this.enemiesUpdate(this.player.position)
 
-        this.coinsUpdate(this.player.position)
-        this.enemiesUpdate(this.player.position)
-
-      setInterval( () =>  {  if ( SPEED >= MAX_SPEED ) SPEED += ACCELERATION; } , 3000);
-
-
+    setInterval( () =>  {  if ( SPEED >= MAX_SPEED ) SPEED += ACCELERATION; } , 3000);
     }
 
     gameLoop(timeStamp: number): any{
@@ -288,12 +310,14 @@ class GameEngine {
 
         this.startProcedure()
         setTimeout( () => {this.countDistance() } , 3000);
-        setTimeout(() => {
-          window.requestAnimationFrame(() => this.gameLoop(Date.now()))
-          this.coinGeneratorInterval = CoinsGenerator.generateCoins(this.coins)
-          this.enemyGeneratorInterval = EnemyGenerator.generateEnemies(this.enemies)
 
-        }, 3000);
+          setTimeout(() => {
+          window.requestAnimationFrame(() => this.gameLoop(Date.now()));
+          let coinFactory = new CoinFactory(MIN_COIN_DELAY, MAX_COIN_DELAY);
+          this.coinGeneratorInterval = new GenerateEntity(coinFactory).generateEntity(this.coins);
+          this.enemyGeneratorInterval = new GenerateEntity(new EnemyFactory(MIN_ENEMY_DELAY, MAX_ENEMY_DELAY)).generateEntity(this.enemies)
+        }, 2000);
+
     }
 
     endGame() {
@@ -310,7 +334,7 @@ class GameEngine {
       scoreEl.style.display = "none";
       countdownEl.style.fontSize = "40px";
       const finalDistance = this.player.distance;
-      const finalScore = coins
+      const finalScore = this.player.coins
       countdownEl.innerHTML =
         `Final distance: ${finalDistance}m` +
         "<br />" +
@@ -318,8 +342,7 @@ class GameEngine {
       this.sendResultToDb(finalScore, finalDistance)
     }
 
-    // post request with coins and points update after game over
-    // updates sidenav coin value
+    // push result to DB
     sendResultToDb(coins, distance): any{
       let xhr = new XMLHttpRequest()
       xhr.onreadystatechange = function() {
@@ -347,4 +370,4 @@ countdownEl.onclick = function () {
 
   playAgainEl.onclick = function () {
   window.location.reload(true);
-};
+}
